ae0420a4574fad3ae748bd3ed7a253dea4c9e78b
Sarah Schultz <sarahjschultz@gmail.com>
2015-08-20 07:32:54 -0600

    really small commit of pseudocode that I don't want to forget

--- lib/parser.rb
+++ lib/parser.rb
@@ -4,6 +4,7 @@ class Parser
     # if perform returns a match
     # scraper will move on to next link
     #regexp: /(\A(ninja)|(top talent)|(guru)|(superstar)\z)/i },
+    # else it will call Job.new and pass the url
   end
 
 end
